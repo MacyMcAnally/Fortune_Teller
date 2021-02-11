1554addb8ca581f65e93ed4328047fe5027e6ce2
Macy M <mei.mcanally@ttu.edu>
2021-02-11 15:02:28 -0600

    Date

--- src/main/java/com/revature/hackathon/model/Fortune.java
+++ src/main/java/com/revature/hackathon/model/Fortune.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,8 +19,8 @@ public class Fortune {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int fortuneMessageId;
-  @CreationTimestamp
-  private LocalDateTime date;
+  @CreatedDate
+  private LocalDate date;
   private String message;
   @ManyToOne
   @JoinColumn(name = "user_id")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ insert into User (username,password) values ('Tian','1234');
 insert into User (username,password) values ('Macy','1234');
 insert into User (username,password) values ('Zack','1234');
 
-insert into Fortune (user_id, message, date) values (1, 'Testing123', current_time);
-insert into Fortune (user_id, message, date) values (2, 'Testing2', current_time);
-insert into Fortune (user_id, message, date) values (3, 'Testing3', current_time);
+insert into Fortune (user_id, message, date) values (1, 'Testing123', current_timestamp);
+insert into Fortune (user_id, message, date) values (2, 'Testing2', current_timestamp);
+insert into Fortune (user_id, message, date) values (3, 'Testing3', current_timestamp);
 insert into Fortune (user_id,message) values (1,'sent a message to Tian');
 insert into Fortune (user_id,message) values (2,'sent a message to Macy');
 insert into Fortune (user_id,message) values (3,'sent a message to Zack');
