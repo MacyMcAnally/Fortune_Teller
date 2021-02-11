1c3a926e1d5889b11667e322e31b873d791c5420
tdeng3 <tdeng3@hawk.iit.edu>
2021-02-11 12:25:52 -0600

    Added getAllFortune endpoint

--- src/main/java/com/revature/hackathon/repo/FortuneRepo.java
+++ src/main/java/com/revature/hackathon/repo/FortuneRepo.java
@@ -3,8 +3,10 @@ package com.revature.hackathon.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.revature.hackathon.model.Fortune;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.revature.hackathon.model.Fortune;
+@Transactional
 public interface FortuneRepo extends JpaRepository<Fortune,Integer> {
 	public List<Fortune> findAll();
 }
--- src/main/java/com/revature/hackathon/service/FortuneService.java
+++ src/main/java/com/revature/hackathon/service/FortuneService.java
@@ -2,12 +2,18 @@ package com.revature.hackathon.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.revature.hackathon.model.Fortune;
 import com.revature.hackathon.repo.FortuneRepo;
+@Service
 
 public class FortuneService {
+	@Autowired
 	private FortuneRepo fortuneRepo;
 	public List<Fortune> findAllFortune() {
+		System.out.println("I am here !!!!!!"+ fortuneRepo.findAll());
 		return fortuneRepo.findAll();
 	}
 
