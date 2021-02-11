0585a87d4d6125ca1ea41fe634df687f8efdb3fc
Macy M <mei.mcanally@ttu.edu>
2021-02-11 14:14:30 -0600

    date is not NULL anymore

---
+++ src/main/java/com/revature/hackathon/model/Fortune.java
@@ -0,0 +1,26 @@
+package com.revature.hackathon.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Fortune {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int fortuneMessageId;
+  @CreationTimestamp
+  private LocalDateTime date;
+  private String message;
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User user;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 insert into User (username,password) values ('Tian','1234');
 insert into User (username,password) values ('Macy','1234');
 insert into User (username,password) values ('Zack','1234');
+
+insert into Fortune (user_id, message, date) values (1, 'Testing123', current_time);
+insert into Fortune (user_id, message, date) values (2, 'Testing2', current_time);
+insert into Fortune (user_id, message, date) values (3, 'Testing3', current_time);
