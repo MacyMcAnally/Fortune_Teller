f4ef596b7d807ee2de096f38aa691b7a8a0a04ca
Macy M <mei.mcanally@ttu.edu>
2021-02-11 16:26:10 -0600

    edit data sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,32 @@ insert into User (username,password) values ('Tian','1234');
 insert into User (username,password) values ('Macy','1234');
 insert into User (username,password) values ('Zack','1234');
 
-insert into Fortune (user_id, message, date) values (1, 'Testing123', current_timestamp);
-insert into Fortune (user_id, message, date) values (2, 'Testing2', current_timestamp);
-insert into Fortune (user_id, message, date) values (3, 'Testing3', current_timestamp);
-insert into Fortune (user_id,message) values (1,'sent a message to Tian');
-insert into Fortune (user_id,message) values (2,'sent a message to Macy');
-insert into Fortune (user_id,message) values (3,'sent a message to Zack');
+insert into Fortune (message, date) values ('Neither Kevin or Johnathan are not going to like your QC answers. Any of them.', current_timestamp);
+insert into Fortune (message, date) values ('The interview you have been preparing for all week is not going to go well, at all.', current_timestamp);
+insert into Fortune (message, date) values ('Not even the God can save you from your next QC', current_timestamp);
+insert into Fortune (message, date) values ('Congratulations! You have been selected for re-panel soon!', current_timestamp);
+insert into Fortune (message, date) values ('Everyone in your batch is going to be selected for an interview, except you!', current_timestamp);
+insert into Fortune (message, date) values ('All your co-workers are soon to be placed on client projects and leave you behind. Good riddance!', current_timestamp);
+insert into Fortune (message, date) values ('Your next interview is entirely on algorithmic time-space complexity calculations. Buckle up!', current_timestamp);
+insert into Fortune (message, date) values ('You thought you did well on your last interview? Your feedback is now a reality check.', current_timestamp);
+insert into Fortune (message, date) values ('No worry, your interviewer did not like you either.', current_timestamp);
+insert into Fortune (message, date) values ('You should have used the Streams API instead of a for loop, better luck on your next coding challenge!', current_timestamp);
+insert into Fortune (message, date) values ('They have decided to go with another candidate.', current_timestamp);
+insert into Fortune (message, date) values ('Your fears are entirely justified', current_timestamp);
+insert into Fortune (message, date) values ('You are going to be relocated to that one place you prayed not to go. God heard you, they just did not care.', current_timestamp);
+insert into Fortune (message, date) values ('The cost of living where you are going is continually increasing"', current_timestamp);
+insert into Fortune (message, date) values ('It is okay, you did fine on the coding and technical interviews... they just did not like you personally.', current_timestamp);
+insert into Fortune (message, date) values ('We realize it is probably best if you do not get a fortune this time, it is not exactly...encouraging', current_timestamp);
+insert into Fortune (message, date) values ('Your future room mate will have less than ideal hygene', current_timestamp);
+insert into Fortune (message, date) values ('Yes, your increasing anxiety is well founded - no, we will not tell you why...you should already know.', current_timestamp);
+insert into Fortune (message, date) values ('Perhaps your imposter syndrome is well founded?', current_timestamp);
+insert into Fortune (message, date) values ('The interviewer was not mad...they were just disappointed', current_timestamp);
+insert into Fortune (message, date) values ('You had something stuck in your teeth for that entire interview... and they definitely noticed.', current_timestamp);
+insert into Fortune (message, date) values ('Your interviewer did their PhD thesis on algorithmic efficiency and is about to take you for a ride.', current_timestamp);
+insert into Fortune (message, date) values ('Relax and just embrace the feeling of helplessness, there are nothing you can do about it', current_timestamp);
+insert into Fortune (message, date) values ('Do not worry you will not get ANY negative feedback, they ghosted you."', current_timestamp);
+
+
+
+
 
