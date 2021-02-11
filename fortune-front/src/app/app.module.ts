efd7967a36b45c5140abfb88ebc466a5a7e13826
main <aquamiguel@gmail.com>
2021-02-11 12:24:17 -0800

    login

--- fortune-front/src/app/app.module.ts
+++ fortune-front/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from '@auth0/auth0-angular';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-mbukza.us.auth0.com',
+      clientId: 'SkCiBeBjwmw87JN02YmBRhH9bHkMaGku'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
